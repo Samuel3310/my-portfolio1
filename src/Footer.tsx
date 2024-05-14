aa3fcb2e982a02cfeee85f69a6d3717b25c28d8a
samuel3310 <awoluowolabisamuel@gmail.com>
2024-05-14 15:52:33 +0100

    personal project done

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -36,19 +36,10 @@ const Footer = () => {
           </a>
         </p>
         <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-          <a href="/" className="text-gray-500">
-            <svg
-              fill="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              className="w-5 h-5"
-              viewBox="0 0 24 24"
-            >
-              <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-            </svg>
-          </a>
-          <a href="/" className="ml-3 text-gray-500">
+          <a
+            href="https://www.github.com/samuel3310"
+            className="ml-3 text-gray-500"
+          >
             <svg
               fill="currentColor"
               strokeLinecap="round"
