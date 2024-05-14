15d42653b1be04605ba60f638642b081e57b55f7
awolu owolabi samuel <58808750+Samuel3310@users.noreply.github.com>
2024-05-14 23:03:56 +0100

    Update Home.tsx

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,5 +1,5 @@
 import { TypeAnimation } from "react-type-animation";
-
+import {Link} from "react-router-dom"
 const Home = () => {
   return (
     <section className="text-gray-600 body-font md:pt-0">
@@ -42,18 +42,18 @@ const Home = () => {
             and inspire!
           </p>
           <div className="flex justify-center">
-            <button
-              type="button"
+            <a 
+             hef="https://drive.google.com/file/d/13O-ko8pNZ7wbn6SLIfesb097MhhbTUrw/view?usp=sharing"
               className="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
             >
               Download CV
             </button>
-            <button
-              type="button"
+            <Link
+              to="/about"
               className="text-white bg-gradient-to-r from-purple-500 to-pri hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
             >
               Explore More
-            </button>
+            </Link>
           </div>
         </div>
         <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 rounded-xl">
