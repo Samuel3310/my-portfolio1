6ba16449e400bd819f664d28da8c581c353eadc8
samuel3310 <awoluowolabisamuel@gmail.com>
2024-05-15 08:12:18 +0100

    Fixed the icon itch

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,5 +1,5 @@
 import { TypeAnimation } from "react-type-animation";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <section className="text-gray-600 body-font md:pt-0">
@@ -42,12 +42,12 @@ const Home = () => {
             and inspire!
           </p>
           <div className="flex justify-center">
-            <a 
-             hef="https://drive.google.com/file/d/13O-ko8pNZ7wbn6SLIfesb097MhhbTUrw/view?usp=sharing"
+            <a
+              href="https://drive.google.com/file/d/13O-ko8pNZ7wbn6SLIfesb097MhhbTUrw/view?usp=sharing"
               className="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
             >
               Download CV
-            </button>
+            </a>
             <Link
               to="/about"
               className="text-white bg-gradient-to-r from-purple-500 to-pri hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
